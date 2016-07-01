4b0d29f71bc5bb553f6b93f1324c507bdfd4265c
Ivan Morgillo <imorgillo@gmail.com>
2016-07-01 13:08:48 +0200

    Add RxImageConverters.uriToFullUri

--- library/src/main/java/com/mlsdev/rximagepicker/RxImageConverters.java
+++ library/src/main/java/com/mlsdev/rximagepicker/RxImageConverters.java
@@ -1,8 +1,10 @@
 package com.mlsdev.rximagepicker;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 
@@ -62,5 +64,41 @@ public class RxImageConverters {
         })
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Observable<String> uriToFullUri(final Context context, final Uri originalUri) {
+        return Observable
+            .create(new Observable.OnSubscribe<String>() {
+                @Override
+                public void call(Subscriber<? super String> subscriber) {
+                    Cursor imageCursor = null;
+                    try {
+                        String imageId = originalUri.getLastPathSegment().split("%3A")[0].split(":")[1];
+                        final String[] imageColumns = {MediaStore.Images.Media.DATA};
+
+                        Uri uri;
+                        if (Environment.getExternalStorageState().equalsIgnoreCase(Environment.MEDIA_MOUNTED)) {
+                            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                        } else {
+                            uri = MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+                        }
+
+                        imageCursor = context.getContentResolver().query(uri, imageColumns, MediaStore.Images.Media._ID + "=" + imageId, null, null);
+                        if (imageCursor != null && imageCursor.moveToFirst()) {
+                            subscriber.onNext(imageCursor.getString(imageCursor.getColumnIndex(MediaStore.Images.Media.DATA)));
+                        } else {
+                            subscriber.onError(new Throwable("No image found"));
+                        }
+                    } catch (Exception e) {
+                        subscriber.onError(e);
+                    } finally {
+                        if (imageCursor != null) {
+                            imageCursor.close();
+                        }
+                    }
+                }
+            })
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread());
     }
 }
